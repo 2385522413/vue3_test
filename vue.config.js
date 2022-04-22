40fd540b55b810062ad35f2e203df765ccaa3b61
杨楠 <2385522413@qq.com>
2022-04-22 22:42:59 +0800

    first day vue3 commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  pages: {
+    index: {
+      //入口
+      entry: 'src/main.js',
+    },
+  },
+  lintOnSave:false,
   transpileDependencies: true
 })
